97260fb765f444133f3860e14a55c02dbd3bbf62
Marceeaax <marcelo.adrian2011@fpuna.edu.py>
2023-09-05 21:53:28 -0400

    agregar produccion diaria

--- gui.py
+++ gui.py
@@ -1,6 +1,14 @@
 from tkinter import *
 from pelu import main
 import customtkinter
+from producciondiaria import calcularproduccion
+from tkinter import filedialog
+
+def select_directory():
+    selected_folder = filedialog.askdirectory(title="Select a Folder")
+    if selected_folder:  # Ensure a folder was selected
+        # Here, you can process the selected folder as required
+        message_label.configure(text=f"Selected folder: {selected_folder}")
 
 def show_message():
     # Hide the main button
@@ -31,8 +39,16 @@ def hide_message():
 
 def show_daily_production():
     # Here, you can add the logic for what happens when this button is pressed
-    message_label.configure(text="Producción Diaria logic not implemented yet.")
+    for widget in root.winfo_children():
+        widget.place_forget()
+
+    output = calcularproduccion()
+
+    message_label.configure(text="")
+    message_label.configure(text=output)
     message_label.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+    return_button.place(relx=0.5, rely=0.6, anchor=CENTER)
 
 customtkinter.set_appearance_mode("dark")
 root = customtkinter.CTk()
@@ -48,9 +64,13 @@ produccion_button = customtkinter.CTkButton(root, text="Producción Diaria", com
 message_label = customtkinter.CTkLabel(root, text="", font=("Arial", 12))
 
 # Return button
-return_button = customtkinter.CTkButton(root, text="Return", command=hide_message)
+return_button = customtkinter.CTkButton(root, text="Volver", command=hide_message)
 
 button.place(relx=0.5, rely=0.45, anchor=CENTER)
 produccion_button.place(relx=0.5, rely=0.55, anchor=CENTER)
+
+select_directory_button = customtkinter.CTkButton(root, text="Escoger directorio", command=select_directory)
+select_directory_button.place(relx=0.5, rely=0.65, anchor=CENTER)  # You can adjust the positioning as needed
+
 
 root.mainloop()
--- producciondiaria.py
+++ producciondiaria.py
@@ -5,6 +5,7 @@ import datetime
 def calcularproduccion():
     
     messages = []
+    viviendas = poblacion = 0
 
     folder_path = "C:\\Users\\Marcelo\\Desktop\\OngoingProjects\\INE"  # Replace with the actual path to your text file
     dat_files = [f for f in os.listdir(folder_path) if f.endswith('.dat')] # List all files in the folder that end with .dat
@@ -20,10 +21,25 @@ def calcularproduccion():
                    and datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(folder_path, f))).date() == today]
 
     if not dat_files_today:
-        messages.append("No .dat files found for today.")
-        
-    
-    print(today)
-    print("PRODUCCION DIARIA")
+        messages.append("No se encontraron archivos editados con fecha de hoy")
+    else:
+        viviendas = poblacion = 0
+        for dat_file in dat_files_today:
+            with open(os.path.join(folder_path, dat_file), 'r', encoding='utf-8-sig') as file:
+                lines = file.readlines()
+                
+                # line[191:199] is the date in format YYYYMMDD
+                # remember that the variable today is in format YYYY-MM-DD so we need to remove the dashes in order to compare them
 
-calcularproduccion()
+                for line in lines:
+                    if line.startswith("1") and today == datetime.datetime.strptime(line[191:199], "%Y%m%d").date():
+                        viviendas += 1
+                        poblacion += int(line[218:222])
+
+        messages.append("PRODUCCION DIARIA")
+        messages.append(f"Fecha: {today}")
+        messages.append(f"Viviendas: {viviendas}")
+        messages.append(f"Población: {poblacion}")
+
+
+    return '\n'.join(messages)
